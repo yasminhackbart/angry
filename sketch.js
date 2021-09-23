a5f988d890e79afbb7ca56db4551257d8726d987
Leticia <leticiadb.santos@gmail.com>
2021-09-23 11:03:33 -0300

    AULA C26 USO GIT

--- sketch.js
+++ sketch.js
@@ -3,7 +3,10 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, box2, pig1;
+var box1, box2,box3,box4,box5;
+var pig1, pig3;
+var bird;
+var log1,log3,log4,log5;
 var backgroundImg;
 var ground;
 function preload() {
@@ -61,47 +64,4 @@ function draw(){
     bird.display();
 }
     
-    ground = new Ground(600,height,1200,20)
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-}
+    
