dd17a0ebaf620d3f04c5226dd27dd5f2ee91f413
yasminhackbart@gmail.com <yasminhackbart@gmail.com>
2021-11-10 15:38:36 -0300

    arquivo sketch

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    plataforma = new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -62,6 +62,4 @@ function draw(){
     log5.display();
 
     bird.display();
-}
-    
-    
+    plataforma.display();
